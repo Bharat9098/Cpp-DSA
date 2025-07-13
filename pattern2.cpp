9e6dd1d1b7632709726f0a3272e7c627d39a541e
Bharat Khatoliya <35236315+Bharat9098@users.noreply.github.com>
2025-07-13 10:29:45 +0530

    Inverted Pyramid pattern

---
+++ pattern2.cpp
@@ -0,0 +1,27 @@
+// * * * * * 
+ // * * * * 
+  // * * * 
+   // * * 
+    // * 
+
+#include<iostream>
+using namespace std;
+
+  void invertedPyramid(int n){
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < i; j++){
+        cout << " ";
+      }
+      for(int k = 0; k < n-i; k++){
+        cout << "* ";
+      }
+      cout << endl;
+    }
+  }
+
+ int main() {
+  
+  int n;
+  cin >> n;
+  invertedPyramid(n);
+}
