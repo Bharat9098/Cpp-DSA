dd940414ae41e67adf16fad4c45842812d64bc50
Bharat Khatoliya <kha.bharat9098@gmail.com>
2025-07-13 10:54:13 +0530

    fix indentation and confugure git from vs code

--- pattern2.cpp
+++ pattern2.cpp
@@ -7,20 +7,19 @@
 #include<iostream>
 using namespace std;
 
-  void invertedPyramid(int n){
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < i; j++){
-        cout << " ";
-      }
-      for(int k = 0; k < n-i; k++){
-        cout << "* ";
-      }
-      cout << endl;
+void invertedPyramid(int n){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < i; j++){
+      cout << " ";
     }
+    for(int k = 0; k < n-i; k++){
+      cout << "* ";
+    }
+    cout << endl;
   }
+}
 
- int main() {
-  
+int main() {
   int n;
   cin >> n;
   invertedPyramid(n);
